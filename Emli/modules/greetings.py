0280d180ea84911256044f192c2776202f95f6db
Darshan <92586012+XDacchuX@users.noreply.github.com>
2022-02-08 10:46:43 +0530

    Update greetings.py

--- Emli/modules/greetings.py
+++ Emli/modules/greetings.py
@@ -1,27 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import asyncio
 import os
 from datetime import datetime
@@ -45,28 +21,9 @@ from wbb.utils.dbfunctions import (captcha_off, captcha_on, del_welcome,
 from wbb.utils.filter_groups import welcome_captcha_group
 from wbb.utils.functions import extract_text_and_keyb, generate_captcha
 
-__MODULE__ = "Greetings"
+__MODULE__ = "Captcha"
 __HELP__ = """
 /captcha [ENABLE|DISABLE] - Enable/Disable captcha.
-
-/set_welcome - Reply this to a message containing correct
-format for a welcome message, check end of this message.
-
-/del_welcome - Delete the welcome message.
-/get_welcome - Get the welcome message.
-
-**SET_WELCOME ->**
-
-The format should be something like below.
-
-```
-**Hi** {name} Welcome to {chat}
-
-~ #This separater (~) should be there between text and buttons, remove this comment also
-
-button=[Duck, https://duckduckgo.com]
-button2=[Github, https://github.com]
-```
 
 **NOTES ->**
 
